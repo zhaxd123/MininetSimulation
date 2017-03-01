96a3d5e960cd43c668bcba58901bf25bd30932a3
Xiaodong Zhang <xizhan@kth.se>
2017-03-01 22:35:45 +0100

    add function to read parameters from configuration file.

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Thesis project of distributed cloud simulation on Mininet with WebRTC traffic.
 
 --2017/2/28
 Using command "cloudtopo.py -d 2 3 4 -c 3" to create three datacenter with 2/3/4 hosts respectively, and three hosts on the client side, each client host connects to one switch.
+
+--2017/3/2
+Add function to read parameters from configuration file.
--- cloudtopo.py
+++ cloudtopo.py
@@ -1,73 +1,70 @@
-import sys
 from mininet.topo import Topo
 from mininet.net import Mininet
 from mininet.cli import CLI
 
-if len(sys.argv) > 3:
 
-	class MyTopo( Topo ):
-	    "Distributed Cloud Topology"
-	    def __init__( self ):
-		"Create custom topo."
-	    	Topo.__init__( self )
+file = open("topo.conf")
+flag = 'OFF'
+for line in file:
+	line_strip = line.strip()
+	if line_strip == "#Datacenter host number":
+		flag = 'datacenter'
+	elif line_strip == "#Client host number":
+		flag = "client"
+	else:
+		if flag == 'datacenter':
+			dc_host_num_list = line_strip.split(',')
+		if flag == "client":
+			cl_host_num = int(line_strip)
+file.close()
 
+
+class MyTopo( Topo ):
+    "Distributed Cloud Topology"
+    def __init__( self ):
+	"Create custom topo."
+    	Topo.__init__( self )
+
+	
+	datacenter_id = 1
+	client_id = 1		
+	host_id = 1
+	dc_switch_list = []
+	dc_gw_list = []
+
+	for i in dc_host_num_list:
+
+		dc_host_num = int(i)
 		
-		flag = 'OFF'		
-		datacenter_id = 1
-		client_id = 1		
-		host_id = 1
-		dc_switch_list = []
-		dc_gw_list = []
+		dc_switch = self.addSwitch('s%d' % (2*datacenter_id - 1))
+		dc_switch_list.append(dc_switch)
 
-		for i in sys.argv:
+		dc_gw = self.addSwitch(('s%d' % (2*datacenter_id)))
+		dc_gw_list.append(dc_gw)
 
-			if i == '-d':
-				flag = 'datacenter'
-				continue
-			if i == '-c':
-				flag = 'client'
-				continue
+		for j in range (1,dc_host_num+1):
+			host = self.addHost('h%d' % host_id ,  ip='10.0.%d.%d/24' % (datacenter_id,j))
+			host_id = host_id + 1
+			self.addLink(host,dc_switch)
 
-			if flag == 'datacenter':
+		datacenter_id = datacenter_id + 1
 
-				dc_host_num = int(i)
-
-				dc_switch = self.addSwitch('s%d' % (2*datacenter_id - 1))
-				dc_switch_list.append(dc_switch)
-
-				dc_gw = self.addSwitch(('s%d' % (2*datacenter_id)))
-				dc_gw_list.append(dc_gw)
-
-				for j in range (1,dc_host_num+1):
-					host = self.addHost('h%d' % host_id ,  ip='10.0.%d.%d/24' % (datacenter_id,j))
-					host_id = host_id + 1
-					self.addLink(host,dc_switch)
-
-				datacenter_id = datacenter_id + 1
+		#Create Clinet hosts
+	for j in range (1,cl_host_num+1):
+		cl_host = self.addHost('h%d' % host_id ,  ip='10.1.%d.1/24' % j)
+		host_id = host_id + 1
+		cl_switch = self.addSwitch('s%d' % (2*(datacenter_id - 1)+j))
+		self.addLink(cl_host,cl_switch)
 
 
-			if flag == 'client':
-
-				cl_host_num = int(i)
-
-				for j in range (1,cl_host_num+1):
-					cl_host = self.addHost('h%d' % host_id ,  ip='10.1.%d.1/24' % j)
-					host_id = host_id + 1
-					cl_switch = self.addSwitch('s%d' % (2*(datacenter_id - 1)+j))
-					self.addLink(cl_host,cl_switch)
+	#Create links between datacenter switches
+	for dc_switch in dc_switch_list:
+		for dc_gw in dc_gw_list:
+			self.addLink(dc_switch,dc_gw)
 
 
 
-		for dc_switch in dc_switch_list:
-			for dc_gw in dc_gw_list:
-				self.addLink(dc_switch,dc_gw)
-
-
-
-	net = Mininet(topo = MyTopo())
-	net.start()
-	CLI(net)
-	net.stop()
-
-else:
-	print "Not enough input variables."
+net = Mininet(topo = MyTopo())
+net.start()
+CLI(net)
+net.stop()
